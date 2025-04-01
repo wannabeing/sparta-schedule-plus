9557673a08e78bd1e238079aa5f3b68e773158e0
wannabeing <wannabeing@naver.com>
2025-04-01 17:07:00 +0900

    feat: 테이블 생성에 필요한 쿼리 추가

---
+++ schema.sql
@@ -0,0 +1,21 @@
+USE schedulePlus;
+
+-- 유저 테이블
+CREATE TABLE user (
+                      id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                      user_name VARCHAR(255) NOT NULL,
+                      email VARCHAR(255) NOT NULL UNIQUE,
+                      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                      updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- 일정 테이블
+CREATE TABLE schedule (
+                          id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                          title VARCHAR(255) NOT NULL,
+                          contents LONGTEXT NOT NULL,
+                          user_id BIGINT,  -- 외래 키
+                          created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                          updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                          FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE  -- user_id 삭제 시 schedule도 삭제
+);
