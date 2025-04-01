5cae9876a5d712051a2caf7a6396900765c85ba3
wannabeing <wannabeing@naver.com>
2025-04-01 17:54:36 +0900

    feat: [Lv2] User DTO 생성

---
+++ src/main/java/org/example/spartascheduleplus/dto/user/UserRepsonseDto.java
@@ -0,0 +1,33 @@
+package org.example.spartascheduleplus.dto.user;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import org.example.spartascheduleplus.entity.Schedule;
+import org.example.spartascheduleplus.entity.User;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class UserRepsonseDto {
+    private final Long id;
+
+    private final String name;
+
+    private final String email;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private final LocalDateTime createdAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private final LocalDateTime updatedAt;
+
+    // ✅ 생성자 (User 객체를 받아 생성)
+    public UserRepsonseDto(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdatedAt();
+    }
+}
---
+++ src/main/java/org/example/spartascheduleplus/dto/user/UserRequestDto.java
@@ -0,0 +1,26 @@
+package org.example.spartascheduleplus.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@JsonIgnoreProperties(ignoreUnknown = false) // 존재하지 않는 필드가 들어오면 무시 false
+@Getter
+public class UserRequestDto {
+    @NotBlank(message = "이름을 입력해주세요.")
+    @Size(max = 20, message = "이름은 20자 이내로 입력해주세요.")
+    private final String name;
+
+    @NotBlank(message = "이메일을 입력해주세요.")
+    @Size(max = 100, message = "이메일은 100자 이내로 입력해주세요.")
+    @Email(message = "유효한 이메일 주소를 입력해주세요.")
+    private final String email;
+
+    // ✅ 생성자
+    public UserRequestDto(String name, String email){
+        this.name = name;
+        this.email = email;
+    }
+}
