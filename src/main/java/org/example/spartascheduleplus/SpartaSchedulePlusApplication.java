af7f5beb8c23274da3082ae3022674a31322b975
wannabeing <wannabeing@naver.com>
2025-04-01 14:22:40 +0900

    chore: 🎉 환경변수 설정 및 기본 프로젝트 구조 추가

---
+++ src/main/java/org/example/spartascheduleplus/SpartaSchedulePlusApplication.java
@@ -0,0 +1,22 @@
+package org.example.spartascheduleplus;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class SpartaSchedulePlusApplication {
+
+    public static void main(String[] args) {
+        // dotenv 환경변수 설정
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+        SpringApplication.run(SpartaSchedulePlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/spartascheduleplus/SpartaSchedulePlusApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.spartascheduleplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpartaSchedulePlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
