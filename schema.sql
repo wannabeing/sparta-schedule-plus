2331a6e1d5db3038034a346377e79de1f7d7907f
wannabeing <wannabeing@naver.com>
2025-04-03 10:56:47 +0900

    feat: [Lv7] 댓글 테이블 생성쿼리 추가

--- schema.sql
+++ schema.sql
@@ -15,8 +15,22 @@ CREATE TABLE schedule (
                           id BIGINT AUTO_INCREMENT PRIMARY KEY,
                           title VARCHAR(255) NOT NULL,
                           contents LONGTEXT NOT NULL,
-                          user_id BIGINT,  -- 외래 키
+                          user_id BIGINT NOT NULL,  -- 외래 키
                           created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
                           updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                          FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE  -- user_id 삭제 시 schedule도 삭제
+
+                          FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE  -- user 삭제 시 schedule도 삭제
+);
+
+-- 댓글 테이블
+CREATE TABLE comment (
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         comment TEXT NOT NULL,
+                         user_id BIGINT NOT NULL,  -- 외래 키
+                         schedule_id BIGINT NOT NULL,  -- 외래 키
+                         created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                         updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+                         FOREIGN KEY (user_id) REFERENCES user(id) ON DELETE CASCADE,  -- user 삭제 시 comment도 삭제
+                         FOREIGN KEY (schedule_id) REFERENCES schedule(id) ON DELETE CASCADE  -- schedule 삭제 시 comment도 삭제
 );
