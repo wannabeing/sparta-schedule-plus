33d3f4c5749d75602d54cbdf1126d0251822b173
wannabeing <wannabeing@naver.com>
2025-04-04 15:29:55 +0900

    refactor: 성공응답 DTO 생성메서드 추가

--- src/main/java/org/example/spartascheduleplus/dto/api/SuccessResponseDto.java
+++ src/main/java/org/example/spartascheduleplus/dto/api/SuccessResponseDto.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
@@ -21,4 +22,30 @@ public class SuccessResponseDto<T> {
 
     @JsonInclude(JsonInclude.Include.NON_NULL) // ✅ null 이면 응답 JSON 에서 생략됨
     private final T data;
+
+
+    /**
+     * 🚀 성공응답 DTO 객체를 생성하는 메서드
+     * @param status httpStatus
+     * @param path 요청 URI
+     * @param message 성공 메시지
+     * @param data 데이터
+     * @return 성공응답 DTO 객체를 반환
+     */
+    public static <T> SuccessResponseDto<T> createSuccessResponseDto(
+            HttpStatus status,
+            String path,
+            String message,
+            T data
+    )
+    {
+        return new SuccessResponseDto<>(
+                LocalDateTime.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                path,
+                message,
+                data
+        );
+    }
 }
