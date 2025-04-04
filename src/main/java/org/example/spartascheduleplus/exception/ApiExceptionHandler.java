54bbbffb1238857fcb5e3414045993daa78e6078
wannabeing <wannabeing@naver.com>
2025-04-04 10:49:44 +0900

    refactor: [Lv5] ApiExceptionHandler 공통 로직 수정

--- src/main/java/org/example/spartascheduleplus/exception/ApiExceptionHandler.java
+++ src/main/java/org/example/spartascheduleplus/exception/ApiExceptionHandler.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestControllerAdvice // @ControllerAdvice + @ResponseBody
 public class ApiExceptionHandler {
     /**
-     * [예외] 유효하지 않은 URL 경로로 접근하려는 경우 호출되는 메서드
+     * [404] 유효하지 않은 URL 경로로 접근하려는 경우 호출되는 메서드
      * @param exception 유효하지 않은 URL 입력 시 발생한 예외 객체
      * @return 에러 메시지와 에러 상태코드 반환
      */
@@ -28,23 +28,20 @@ public class ApiExceptionHandler {
     public ResponseEntity<ErrorResponseDto> handleNotFoundUrlException(
             NoHandlerFoundException exception)
     {
-        ErrorResponseDto responseDto = new ErrorResponseDto(
-                LocalDateTime.now(),
-                HttpStatus.NOT_FOUND.value(),
-                HttpStatus.NOT_FOUND.getReasonPhrase(),
+        return createErrorResponse(
+                HttpStatus.NOT_FOUND,
                 exception.getRequestURL(),
                 "유효하지 않은 URL 입니다. 다시 한번 확인해주세요.",
                 null
         );
-        return new ResponseEntity<>(responseDto, HttpStatus.NOT_FOUND);
     }
 
     /**
-     * [예외] 요청 데이터 유효성 검사 실패시 호출되는 메서드
+     * [400] 요청 데이터 유효성 검사 실패시 호출되는 메서드
      * @param exception 유효성 검사 실패로 발생한 예외 객체
      * @return 유효성 검사 실패한 필드와 메시지, 에러 상태 코드 반환
      */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
     public ResponseEntity<ErrorResponseDto> handleValidationException(
             MethodArgumentNotValidException exception,
             HttpServletRequest httpRequest)
@@ -59,19 +56,16 @@ public class ApiExceptionHandler {
                 ))
                 .toList();
 
-        ErrorResponseDto responseDto = new ErrorResponseDto(
-                LocalDateTime.now(),
-                HttpStatus.NOT_FOUND.value(),
-                HttpStatus.NOT_FOUND.getReasonPhrase(),
+        return createErrorResponse(
+                HttpStatus.BAD_REQUEST,
                 httpRequest.getRequestURI(),
                 "유효성 검사에 실패하였습니다.",
                 fieldErrors
         );
-        return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
     }
 
     /**
-     * [예외] 서비스 또는 컨트롤러에서 예외를 던졌을 경우 호출되는 메서드
+     * [400] 서비스 또는 컨트롤러에서 예외를 던졌을 경우 호출되는 메서드
      * @param exception 호출된 예외 객체
      * @return 에러 메시지와 에러 상태코드 반환
      */
@@ -86,16 +80,13 @@ public class ApiExceptionHandler {
             status = HttpStatus.BAD_REQUEST; // null 일 경우, 기본 값 설정
         }
 
-
-        // 기본 메시지 설정
-        String message = "유효하지 않은 요청입니다.";
-
         // 상태 코드에 따라 메시지 설정
-        switch (status) {
-            case FORBIDDEN -> message = "접근 권한이 없습니다.";
-            case NOT_FOUND -> message = "요청하신 데이터를 찾을 수 없습니다.";
-            case CONFLICT -> message = "충돌되는 요청입니다.";
-        }
+        String message = switch (status) {
+            case FORBIDDEN -> "접근 권한이 없습니다.";
+            case NOT_FOUND -> "요청하신 데이터를 찾을 수 없습니다.";
+            case CONFLICT -> "충돌되는 요청입니다.";
+            default -> "유효하지 않은 요청입니다.";
+        };
 
         // 에러 필드 메시지 리스트
         List<ErrorResponseDto.FieldError> fieldErrors = List.of(
@@ -104,19 +95,17 @@ public class ApiExceptionHandler {
                 )
         );
 
-        ErrorResponseDto responseDto = new ErrorResponseDto(
-                LocalDateTime.now(),
-                status.value(),
-                status.getReasonPhrase(),
+        return createErrorResponse(
+                status,
                 httpRequest.getRequestURI(),
                 message,
                 fieldErrors
         );
-        return new ResponseEntity<>(responseDto, HttpStatus.NOT_FOUND);
     }
 
+
     /**
-     * [예외] 파라미터 타입을 전달 받았을 경우 호출되는 메서드
+     * [400] 파라미터 타입을 전달 받았을 경우 호출되는 메서드
      * @param exception 파라미터 타입을 전달 받았을 경우에 발생한 예외 객체
      * @return 에러 메시지와 에러코드 반환
      */
@@ -135,19 +124,16 @@ public class ApiExceptionHandler {
                 )
         );
 
-        ErrorResponseDto responseDto = new ErrorResponseDto(
-                LocalDateTime.now(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+        return createErrorResponse(
+                HttpStatus.BAD_REQUEST,
                 httpRequest.getRequestURI(),
                 "유효하지 않은 입력 값입니다.",
                 fieldErrors
         );
-        return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
     }
 
     /**
-     * [예외] 사용자 요청 JSON 을 제대로 읽을 수 없을 때 호출되는 메서드
+     * [400] 사용자 요청 JSON 을 제대로 읽을 수 없을 때 호출되는 메서드
      * @param exception 요청 JSON 을 읽을 수 없을 때 호출되는 예외 객체
      * @return 에러 메시지와 에러코드 반환
      */
@@ -171,19 +157,16 @@ public class ApiExceptionHandler {
                 )
         );
 
-        ErrorResponseDto responseDto = new ErrorResponseDto(
-                LocalDateTime.now(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+        return createErrorResponse(
+                HttpStatus.BAD_REQUEST,
                 httpRequest.getRequestURI(),
                 "RequestBody JSON 형식이 올바르지 않습니다. 형식을 다시 확인해주세요.",
                 fieldErrors
         );
-        return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
     }
 
     /**
-     * [예외] 요청한 정보가 이미 존재할 때 호출되는 메서드 (ex. 이메일 중복)
+     * [409] 요청한 정보가 이미 존재할 때 호출되는 메서드 (ex. 이메일 중복)
      * @param exception 중복한 값이 있을 때 호출되는 예외 객체
      * @param httpRequest Http 요청 객체
      * @return API 에러 응답 객체 반환
@@ -205,16 +188,12 @@ public class ApiExceptionHandler {
                 new ErrorResponseDto.FieldError("error", message)
         );
 
-        ErrorResponseDto responseDto = new ErrorResponseDto(
-                LocalDateTime.now(),
-                HttpStatus.CONFLICT.value(),
-                HttpStatus.CONFLICT.getReasonPhrase(),
+        return createErrorResponse(
+                HttpStatus.CONFLICT,
                 httpRequest.getRequestURI(),
                 "요청이 충돌하였습니다.",
                 fieldErrors
         );
-
-        return new ResponseEntity<>(responseDto, HttpStatus.CONFLICT);
     }
 
     /**
@@ -239,15 +218,37 @@ public class ApiExceptionHandler {
                 )
         );
 
-        ErrorResponseDto responseDto = new ErrorResponseDto(
-                LocalDateTime.now(),
-                HttpStatus.BAD_REQUEST.value(),
-                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+        return createErrorResponse(
+                HttpStatus.BAD_REQUEST,
                 httpRequest.getRequestURI(),
                 "유효하지 않은 입력 값입니다.",
                 fieldErrors
         );
-        return new ResponseEntity<>(responseDto, HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 🚀 공통 예외 응답객체 생성 메서드
+     * @param status HttpStatus
+     * @param path 사용자 요청 URI
+     * @param message 에러 메시지
+     * @param fieldErrors 상세 에러 내용
+     * @return 에러응답객체 및 상태코드 반환
+     */
+    private ResponseEntity<ErrorResponseDto> createErrorResponse(
+            HttpStatus status,
+            String path,
+            String message,
+            List<ErrorResponseDto. FieldError> fieldErrors
+    ){
+        ErrorResponseDto response = new ErrorResponseDto(
+                LocalDateTime.now(),
+                status.value(),
+                status.getReasonPhrase(),
+                path,
+                message,
+                fieldErrors
+        );
+        return new ResponseEntity<>(response, status);
     }
 
 }
