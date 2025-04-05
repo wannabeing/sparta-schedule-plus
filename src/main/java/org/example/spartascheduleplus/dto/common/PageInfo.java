75645a790d59c679a25a8d0e1738f049e3a1f111
wannabeing <wannabeing@naver.com>
2025-04-05 16:58:38 +0900

    chore: Page 정보 변수명 수정

--- src/main/java/org/example/spartascheduleplus/dto/common/PageInfo.java
+++ src/main/java/org/example/spartascheduleplus/dto/common/PageInfo.java
@@ -6,8 +6,8 @@ import org.springframework.data.domain.Page;
 @Getter
 public class PageInfo {
     private final int totalCount; // 전체 항목 수
-    private final int pages; // 전체 페이지 개수
-    private final int number; // 현재 페이지 번호
+    private final int totalPages; // 전체 페이지 개수
+    private final int currentPage; // 현재 페이지 번호
     private final int size; // 한 페이지당 보여주는 항목 수
 
     private final boolean hasNext; // 다음 페이지 존재여부
@@ -18,8 +18,8 @@ public class PageInfo {
     public PageInfo(Page<?> page)
     {
         this.totalCount = (int) page.getTotalElements();
-        this.pages = page.getTotalPages();
-        this.number = page.getNumber();
+        this.totalPages = page.getTotalPages();
+        this.currentPage = page.getNumber();
         this.size = page.getSize();
         this.hasNext = page.hasNext();
         this.hasPrevious = page.hasPrevious();
